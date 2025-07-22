c3ca83e56ebd0f95c79238e002342c929001ff60
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-07-22 10:00:19 +0530

    Time: 0 ms (100%), Space: 54.4 MB (9.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,17 +4,12 @@
  */
 var climbStairs = function(n) {
     let N = n+1
-  if (N === 0) return 1;
-  if (N === 1) return 1;
-
   let prev = 0;
   let curr = 1;
-
   for (let i = 2; i <= N; i++) {
     let next = prev + curr;
     prev = curr;
     curr = next;
   }
-
   return curr;
 };
