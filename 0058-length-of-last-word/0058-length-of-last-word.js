a1f62964aa7afbc76ef227235017d172b802a9ba
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-08-07 10:53:54 +0530

    Time: 0 ms (100%), Space: 54.2 MB (19.08%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -4,13 +4,17 @@
  */
 var lengthOfLastWord = function(s) {
     let i = s.length-1;
+
     while(i>=0 && s[i] === ' '){
         i--;
     }
+
     let length = 0;
+
     while(i>=0 && s[i] !== ' '){
         length++;
         i--;
     }
+    
     return length
 };
