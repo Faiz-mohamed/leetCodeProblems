742af07ce45b1bd4c1c8140731dba7338aa3b374
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-09 16:59:42 +0530

    Time: 0 ms (100%), Space: 54.5 MB (12.1%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.js
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.js
@@ -2,10 +2,4 @@
  * @param {number} n
  * @return {number}
  */
-var smallestEvenMultiple = function(n) {
-    if(n%2 === 0 ){
-        return n 
-    }else{
-        return n*2
-    }
-};
+var smallestEvenMultiple = n => n % 2 === 0 ? n : n * 2
