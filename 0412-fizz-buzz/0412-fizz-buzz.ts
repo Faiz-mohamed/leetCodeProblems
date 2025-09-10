624d12c3283d1709f068a4df805570cc8ccdd0f3
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-10 12:19:33 +0530

    Time: 2 ms (40.67%), Space: 58.5 MB (70.34%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.ts
@@ -0,0 +1,21 @@
+function fizzBuzz(n: number): string[] {
+    let arr: string[] = [];
+    const DivBoth = (i) => i % 3 === 0 && i % 5 === 0;
+    const DivThree = (i) => i % 3 === 0;
+    const DivFive = (i) => i % 5 === 0;
+    for (let i = 1; i <= n; i++) {
+        if (DivBoth(i)) {
+            arr.push("FizzBuzz")
+        }
+        else if (DivThree(i)) {
+            arr.push("Fizz")
+        }
+        else if (DivFive(i)){
+            arr.push("Buzz")
+        }
+        else{
+            arr.push(`${i}`)
+        }
+    }
+    return arr;
+};
