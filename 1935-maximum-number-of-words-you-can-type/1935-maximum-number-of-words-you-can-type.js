04fd8cfc077451386fa5759eb40f882dce35f613
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-17 09:58:57 +0530

    2 ms (74.78%)

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.js
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.js
@@ -1,14 +1,8 @@
-/**
- * @param {string} text
- * @param {string} brokenLetters
- * @return {number}
- */
 var canBeTypedWords = function (text, brokenLetters) {
     const words = text.split(' ');
-    const letters = brokenLetters.split('')
     let counter = words.length;
     for (word of words){
-        for(l of letters){
+        for(l of brokenLetters){
             if(word.includes(l)){
                 counter--
                 break;
