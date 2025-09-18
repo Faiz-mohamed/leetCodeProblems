d8ebab8009e90245e4465a1d80b9d304c1ed181b
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-18 09:48:25 +0530

    11 ms (25.98%)

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function (nums) {
+    let result = []
+    for (let i = 0; i < nums.length; i++) {
+        result.push(0)
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                result[i]++
+            }
+        }
+    }
+    return result
+};
