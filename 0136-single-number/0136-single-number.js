d8dfcffd8163590f603fa03d9081e9e4354f5a9d
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-20 10:00:59 +0530

    14 ms (20.87%)

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,9 @@
+var singleNumber = function (nums) {
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 1; i += 2) {
+        if (nums[i] !== nums[i + 1]) {
+            return nums[i]
+        }
+    }
+    return nums[nums.length - 1]
+};
