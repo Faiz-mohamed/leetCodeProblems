0faf82a4b9bd0d0fab94209b59708bb3628e9a00
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-21 17:03:45 +0530

    4 ms (80.46%)

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.js
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.js
@@ -3,8 +3,10 @@ var differenceOfSum = function (nums) {
     let Dsum = 0;
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
-        Dsum += nums[i].toString().split('').map(Number)
-        .reduce((a, c) => a + c)
+        let temp = nums[i].toString();
+        for(n of temp){
+            Dsum += Number(n)
+        }
     }
     return Math.abs(sum - Dsum)
 };
