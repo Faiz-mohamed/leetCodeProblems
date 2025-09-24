edf5a4baf5c65fee3638323f97ce0433b86dddc4
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-24 10:23:26 +0530

    5 ms (8.8%)

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.js
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.js
@@ -1,16 +1,8 @@
 var decompressRLElist = function (nums) {
     let arr = [];
-    let freq = 0;
-    let val = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (i % 2 === 0) {
-            freq = nums[i]
-            console.log(arr)
-        } else {
-            val = nums[i]
-            for (let j = 0; j < freq ; j++){
-                arr.push(val)
-            }
+    for (let i = 0; i < nums.length; i += 2) {
+        for (let j = 0; j < nums[i]; j++){
+            arr.push(nums[i + 1])
         }
     }
     return arr
