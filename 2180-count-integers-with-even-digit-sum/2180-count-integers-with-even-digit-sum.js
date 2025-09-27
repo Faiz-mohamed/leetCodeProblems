22d86714325af3eeb1f010479196bfd372a86ffa
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-09-27 14:23:41 +0530

    6 ms (23.04%)

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -1,12 +1,8 @@
-/**
- * @param {number} num
- * @return {number}
- */
 var countEven = function (num) {
     let count = 0;
     for (let i = 1; i <= num; i++) {
-        let digitSum = i.toString().split('').map(Number)
-            .reduce((a, b) => a + b)
+        let digitSum = i.toString().split('')
+        .reduce((a, b) => a + Number(b),0)
         if (digitSum % 2 === 0) {
             count++
         }
