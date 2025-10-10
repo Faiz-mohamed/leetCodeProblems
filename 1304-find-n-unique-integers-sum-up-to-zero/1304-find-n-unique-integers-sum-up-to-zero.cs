6c7cd913c5bc3cbbf795f7331499ecd6d4b1bec6
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-10-10 10:54:54 +0530

    0 ms (100%)

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] SumZero(int n) {
+        int[] arr = new int[n];
+        int idx = 0;
+        for(int i = 1; i <= n/2; i++){
+            arr[idx++] = i;
+            arr[idx++] = -i;
+        }
+        if(n % 2 != 0) arr[idx] = 0;
+        return arr;
+    }
+}
