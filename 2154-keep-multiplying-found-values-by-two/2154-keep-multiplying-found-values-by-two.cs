9c3ba6b84bcf08283b7539b37b5259f46eeccbdb
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-10-15 22:47:18 +0530

    0 ms (100%)

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int FindFinalValue(int[] nums, int original) {
+        while(nums.Contains(original))
+        {
+            original = original*2;
+        };
+        return original;
+    }
+}
