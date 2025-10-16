eb18b38a43f513c4d068d3871168c0304df304f0
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-10-16 09:53:42 +0530

    1034 ms (62.15%)

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -1,6 +1,6 @@
 var checkPerfectNumber = function (num) {
     let sum = 0;
-    for (let i = 1; i < num; i++) {
+    for (let i = 1; i <= num / 2; i++) {
         if (num % i === 0) sum += i;
     }
     return sum === num;
