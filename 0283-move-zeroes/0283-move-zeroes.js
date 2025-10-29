248d0af988fa9262fadceafec24094cf5fb5d270
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-10-29 10:22:41 +0530

    Time: 4 ms (30.1%), Space: 59.9 MB (53.57%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -1,16 +1,9 @@
-var moveZeroes = function(nums) {
-    let arr = []
-    let count = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] == 0){
-            count++
-        } else {
-            arr.push(nums[i])
+var moveZeroes = function (nums) {
+    let left = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[left], nums[i]] = [nums[i], nums[left]]
+            left++
         }
-    }    
-    for(let j = 0; j < count; j++){
-        arr.push(0)
     }
-    nums.length = 0;
-    nums.push(...arr)
 };
