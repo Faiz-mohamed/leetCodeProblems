db52c91e63c39f707d768d35d1b501bc5b21d321
Faiz-mohamed <faizmohamed3336@gmail.com>
2025-11-09 19:11:09 +0530

    Time: 22 ms (70.62%), Space: 29.3 MB (14.79%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cs
@@ -0,0 +1,16 @@
+public class Solution : GuessGame {
+    
+    public int GuessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            int val = guess(mid);
+            if(val == 0) return mid;
+            else if (val == -1) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+}
